ae9aad34e3ab0a1d8087fc781402626d34b2e7b9
Senze <317277707+notSENZE@users.noreply.github.com>
2026-09-16 09:33:01 +0200

    0.9.0-beta.2 - Update quest maps and default spawns

--- ArmyOfTwo.Server/ModInfo.cs
+++ ArmyOfTwo.Server/ModInfo.cs
@@ -5,7 +5,7 @@ internal static class ModInfo
     public const string Guid = "spt.senze.armyoftwo";
     public const string Name = "Army of Two";
     public const string Author = "SENZE";
-    public const string Version = "0.9.0-beta.1";
+    public const string Version = "0.9.0-beta.2";
 
     public const int RookWildSpawnType = 658400;
     public const int TombstoneWildSpawnType = 658401;
