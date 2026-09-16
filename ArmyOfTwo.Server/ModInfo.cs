317ffe82a8e087af64fc9d4358c491d1d30e6361
Senze <317277707+notSENZE@users.noreply.github.com>
2026-09-16 08:30:41 +0200

    0.9.0-beta.1 - Prepare public beta release

--- ArmyOfTwo.Server/ModInfo.cs
+++ ArmyOfTwo.Server/ModInfo.cs
@@ -5,7 +5,7 @@ internal static class ModInfo
     public const string Guid = "spt.senze.armyoftwo";
     public const string Name = "Army of Two";
     public const string Author = "SENZE";
-    public const string Version = "0.8.5";
+    public const string Version = "0.9.0-beta.1";
 
     public const int RookWildSpawnType = 658400;
     public const int TombstoneWildSpawnType = 658401;
